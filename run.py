40033f5374554f5a66615d9dd9e5aa0ce731a436
Ci Zhang <zhangci@yahoo.com>
2020-05-04 00:06:19 +0800

    updates

--- run.py
+++ run.py
@@ -15,8 +15,8 @@ end_year = 2015
 end_month = 1
 end_day = 2
 
-nc_path = '/home/zhangc/scenariomip_cmip6/nc_data/'
-im_path = '/home/zhangc/scenariomip_cmip6/im_data/'
+nc_path = '/home/zhangc/cesm2_cmip6/nc_data/'
+im_path = '/home/zhangc/cesm2_cmip6/im_data/'
 os_path = '/home/zhangc/repositories/nc2im_cesm2/'
 
 ncs = ['ta_6hrLev_CESM2_ssp245_r2i1p1f1_gn_201501010000-202412311800.nc',     #  0
@@ -32,6 +32,7 @@ ncs = ['ta_6hrLev_CESM2_ssp245_r2i1p1f1_gn_201501010000-202412311800.nc',     # 
        'USGS-gtopo30_0.9x1.25_remap_c051027.nc']                              # 10
 
 chdir(nc_path)
+
 dss = []
 for nc in ncs:
     ds = open_dataset(nc)
@@ -39,7 +40,6 @@ for nc in ncs:
 
 start_date = DatetimeNoLeap(start_year, start_month, start_day)
 end_date = DatetimeNoLeap(end_year, end_month, end_day)
-
 dates = []
 date = start_date
 while date <= end_date:
@@ -63,7 +63,7 @@ for date in dates:
     tera = datetime64('2015-'+str(tnum[1]).zfill(2)+'-01')
     file_time = str(t6hr).replace(' ', '_')
     # Land-Sea Mask
-    v = dss[9].LANDMASK.values*1.0
+    v = dss[9].LANDMASK.values
     da = DataArray(name='ls', data=float64(v))
     file_name = 'ls_' + file_time + '.nc'
     da.to_netcdf(im_path+file_name)
@@ -73,11 +73,9 @@ for date in dates:
     file_name = 'sh_' + file_time + '.nc'
     da.to_netcdf(im_path+file_name)
     # 3D Temperature, Humidity, Winds
-    das = []
     for i in [0, 1, 2, 3]:
         ds = dss[i].sel(time=t6hr).sortby('lev')
         vi = ds.variable_id
-        das.append(ds[vi])
         v = ds[vi]
         v.attrs = {}
         v.values = float64(v.values)
@@ -85,21 +83,15 @@ for date in dates:
         file_name = vi + '_' + file_time + '.nc'
         da.to_netcdf(im_path+file_name)
     # Surface Pressure
-    ds = dss[4].sel(time=t6hr)
-    v = ds['ps'].values
+    v = dss[4].ps.sel(time=t6hr).values
     da = DataArray(name='ps', data=float64(v))
     file_name = 'ps_' + file_time + '.nc'
     da.to_netcdf(im_path+file_name)
-    # Virtual Temperature
-    t = das[0].sortby('lev').values
-    q = das[1].sortby('lev').values
+    # 3D Virtual Temperature
+    t = dss[0].ta.sel(time=t6hr).sortby('lev').values
+    q = dss[1].hus.sel(time=t6hr).sortby('lev').values
     tv = t*(1.+q*0.61)
-    lev = -das[0].sortby('lev').lev.values
-    lat = das[0].lat.values
-    lon = das[0].lon.values
-    da = DataArray(name='tv', data=tv, coords=[lev, lat, lon], dims=['lev', 'lat', 'lon']).transpose('lat', 'lon', 'lev')
-    v = da.values
-    da = DataArray(name='tv', data=float64(v))
+    da = DataArray(name='tv', data=float64(tv)).transpose('dim_1', 'dim_2', 'dim_0')
     file_name = 'tv_' + file_time + '.nc'
     da.to_netcdf(im_path+file_name)
     # 3D Pressure
@@ -107,25 +99,23 @@ for date in dates:
     p3 = empty(shape)
     for i in range(len(lev)):
         p3[i] = full(shape[1:3], lev[i])
-    da = DataArray(name='p3', data=p3, coords=[lev, lat, lon], dims=['lev', 'lat', 'lon']).transpose('lat', 'lon', 'lev')
-    v = da.values
-    da = DataArray(name='p3', data=float64(v))
+    da = DataArray(name='p3', data=p3).transpose('dim_1', 'dim_2', 'dim_0')
     file_name = 'p3_' + file_time + '.nc'
     da.to_netcdf(im_path+file_name)
     # 2M Temperature, Humidity, Winds
     vis = ['t2', 'q2', 'u2', 'v2']
     for i in range(4):
-        v = das[i].isel(lev=-1).values
+        v = dss[i][vis[i]].isel(lev=-1).values
         da = DataArray(name=vis[i], data=float64(v))
         file_name = vis[i] + '_' + file_time + '.nc'
         da.to_netcdf(im_path+file_name)
     # Skin Temperature
     v = dss[7].sel(time=tmon)['ts'].values
     da = DataArray(name=vi, data=float64(v))
-    file_name = vi + '_' + file_time + '.nc'
+    file_name = 'ts_' + file_time + '.nc'
     da.to_netcdf(im_path+file_name)
     # Sea Level Pressure
-    da = das[0].isel(lev=-1)
+    da = dss[0].isel(lev=-1)
     lev = da.lev.values*-100
     v = full(da.shape, lev)
     da = DataArray(name='p2', data=float64(v))
@@ -163,9 +153,9 @@ for date in dates:
         da = DataArray(name=vi, data=float64(v))
         file_name = vi + '_' + file_time + '.nc'
         da.to_netcdf(im_path+file_name)
-    # run NCL
+    # Run NCL
     chdir(os_path)
     command = 'ncl convert_nc_to_im.ncl ' + "'file_time=" + '"' + file_time + '"' + "'"
     system(command)
-    # flag
+    # Flag
     print('The '+str(date)+' IM File Has Been Processed!')
