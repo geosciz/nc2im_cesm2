201dcb70a154cef0741247ae649d45d3310691e1
Ci Zhang <zhangci@yahoo.com>
2020-05-05 21:28:22 +0800

    updates

---
+++ bsub_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#BSUB -q cweg
+#BSUB -J run_test
+#BSUB -m cn16
+#BSUB -n 1
+#BSUB -o /home/zhangc/bsub_logs/out
+#BSUB -e /home/zhangc/bsub_logs/err
+
+cd /home/zhangc/repositories/nc2im_cesm2/
+python run.py
